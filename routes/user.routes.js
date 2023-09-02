617df72688b2f424dc56a77fc20e9404ef18e5de
RimmaBuzuluk <rimmysya2000@gmail.com>
2023-09-02 21:52:41 +0300

    post routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,8 @@
+
 const Router = require('express');
 const router = new Router();
-const userController = require('../controller/user.controller'); // Исправлено на controller
+const userController = require('../controller/user.controller'); 
+
 
 router.post('/user', userController.createUser);
 router.get('/user', userController.getUser);
